1a4ada0d7b5001d945ce323feb95844c1cb606e2
ArtemixArt228 <artemix.portfolio@gmail.com>
2024-10-17 18:17:57 +0300

    fix: Fixed type error service

--- client/src/services/chatMessage/index.service.ts
+++ client/src/services/chatMessage/index.service.ts
@@ -1,36 +1,49 @@
-import { HttpService } from "../http.service";
-import { HttpServiceFactory } from "../index";
+import { HttpService } from '../http.service';
+import { HttpServiceFactory } from '../index';
 
-import { IResponse } from "../../types";
+import { IResponse } from '../../types';
 
-import { ICreateMessageParams, IGetMessagesParams, IGetMessagesResponse } from "./index.type";
+import {
+  ICreateMessageParams,
+  IGetMessagesParams,
+  IGetMessagesResponse,
+} from './index.type';
 
 export class ChatMessageService {
-    constructor(private httpService: HttpService) {}
+  constructor(private httpService: HttpService) {}
 
-    public async createMessage({ sender, content, groupId, isVoiceMessage, fileURL }: ICreateMessageParams) {
-        try {
-            const response = await this.httpService.post<IResponse, ICreateMessageParams>(
-                "messages",
-                { sender, content, groupId, isVoiceMessage, fileURL }
-            );
-            return { response };
-        } catch (error) {
-            return { error };
-        }
-    }
+  public async createMessage({
+    sender,
+    content,
+    groupId,
+    isVoiceMessage,
+    file,
+  }: ICreateMessageParams) {
+    try {
+      const response = await this.httpService.post<
+        IResponse,
+        ICreateMessageParams
+      >('messages', { sender, content, groupId, isVoiceMessage, file });
 
-    public async getMessages({ groupId }: IGetMessagesParams) {
-        try {
-            const response = await this.httpService.get<IResponse<IGetMessagesResponse>>(
-                `messages/${groupId}`
-            );
-            return { response };
-        } catch (error) {
-            return { error };
-        }
+      return { response };
+    } catch (error) {
+      return { error };
     }
+  }
+
+  public async getMessages({ groupId }: IGetMessagesParams) {
+    try {
+      const response = await this.httpService.get<
+        IResponse<IGetMessagesResponse>
+      >(`messages/${groupId}`);
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  }
 }
 
 const factory = new HttpServiceFactory();
-export const chatMessageService = new ChatMessageService(factory.createHttpService());
+export const chatMessageService = new ChatMessageService(
+  factory.createHttpService(),
+);
